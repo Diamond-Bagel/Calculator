d6d39882b43f78aeae88bdce125a676f4996da1f
Diamond-Bagel <72178852+Diamond-Bagel@users.noreply.github.com>
2020-10-15 14:19:51 -0700

    Add files via upload
    Yeah, it's not very good, but pls don't judge too harshly.

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float x;
+    float y;
+
+    cout << "Please enter a number: ";
+    cin >> x;
+    cout << "Please enter another number: ";
+    cin >> y;
+    float z = x + y;
+    cout << "Your final number is " << z;
+    return 0;
+}
