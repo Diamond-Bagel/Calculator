37fd33304b5edfcc4af9e1546f48767fa24566c8
Diamond-Bagel <72178852+Diamond-Bagel@users.noreply.github.com>
2021-01-01 13:20:53 -0700

    Create calculator.c
    Basic calculator in C

---
+++ calculator.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(void)
+{
+    double x, y;
+    // For choosing what type of equation to do
+    char math_choice;
+    // For choosing whether or not to calculate an expression
+    char choice;
+    printf("Would you like to calculate an equation?(y/n)\n");
+    choice = getch();
+
+    do
+    {
+        printf("\n1)\tAdd\n2)\tSubtract\n3)\tDivide\n4)\tMultiply\n5)\tExponent\n6)\tSqrt\n\n");
+        math_choice = getch();
+        if (math_choice == '6')
+        {
+            printf("Enter your number: ");
+            scanf("%lf", &x);
+            printf("%lf\n", sqrt(x));
+        }
+        else
+        {
+            printf("First number: ");
+            scanf("%lf", &x);
+
+            printf("Second number: ");
+            scanf("%lf", &y);
+            switch (math_choice)
+            {
+            case '1':
+                printf("\n%lf\n", x + y);
+                break;
+            case '2':
+                printf("%lf\n", x - y);
+                break;
+            case '3':
+                printf("%lf\n", x / y);
+                break;
+            case '4':
+                printf("%lf\n", x * y);
+                break;
+            case '5':
+                printf("%lf\n", pow(x, y));
+                break;
+            }
+        }
+        printf("Would you like to calculate another equation?(y/n)\n");
+        choice = getch();
+    } while (choice == 'y');
+    return 0;
+}
+
